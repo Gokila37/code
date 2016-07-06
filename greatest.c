b179d9f9d44e9e60837eeab9f6897445beb0cebc
Gokila37 <gokilapv3@gmail.com>
2016-07-06 14:37:16 +0530

    Create greatest.c

---
+++ greatest.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+clrscr();
+printf("\n enter three numbers"
+scanf("%d%d%d",&a,&b,&c);
+if(a>b && a>c)
+{
+printf("\n a is greatest number");
+}
+ifelse(b>c && b>a)
+{
+printf("\n b is greatest number");
+}
+else
+{
+printf("\n c is greatest number");
+}
+getch();
+}
