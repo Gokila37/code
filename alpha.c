9d961164b648e38763ddbfd541b1cf9561600916
Gokila37 <gokilapv3@gmail.com>
2016-07-06 15:05:06 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int value,a,z;
+clrscr();
+printf("\n check the number is character or not");
+printf("\n enter the character");
+scanf("%d",&a);
+if(value<=a && value>=z)
+{
+printf("\n this is alphabetic");
+}
+else
+{
+printf("\n this is not an alphabetic");
+}
+getch();
+}
